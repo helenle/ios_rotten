4b902cac3952fd16e8bf1660cabdafb143133c4c
helenle <helenle_8@yahoo.com>
2014-10-02 22:07:53 -0700

    rotten - first commit

---
+++ rotten/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  rotten
+//
+//  Created by Huyen Le on 9/30/14.
+//  Copyright (c) 2014 Helen Le. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
